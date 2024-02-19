95dcfa4b230b4c202695cbac093853d3319b6ccb
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-19 19:35:21 +0100

    create nav Data

---
+++ data/navData.js
@@ -0,0 +1,29 @@
+const navData = [
+  {
+      href: '/',
+      name: 'Accueil',
+      burgerMenuName:'Accueil',
+  },
+  {
+      href: '/histoire',
+      name: 'Histoire',
+      burgerMenuName:'Mon Histoire',
+  },
+  {
+      href: '/projets',
+      name: 'Projets',
+      burgerMenuName:'Mes Projets',
+  },
+  {
+      href: '/skills',
+      name: 'Skills',
+      burgerMenuName:'Mes Skills',
+  },
+  {
+      href: '/contact',
+      name: 'Contact',
+      burgerMenuName:'Contact',
+  },
+];
+
+export default navData;
