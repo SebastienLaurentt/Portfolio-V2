eafbeab4fb76073b9ad7a8e1422d2a96830b4d82
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-19 18:22:31 +0100

    add lorem content in home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import Section from "@/components/ui/Section/Section";
+
 export default function Home() {
-  return <div>yoo</div>;
+  return (
+    <main className="mb-20 md:mb-24 lg:mb-32">
+      <Section marginBottom={true} marginTop={true}> 
+
+        <div className="w-1/2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, quaerat amet repellat exercitationem illo et consequuntur rem, aliquam quibusdam, excepturi unde suscipit expedita! Perferendis eveniet maiores numquam commodi nostrum dolorem iusto, laudantium aspernatur quae assumenda hic sunt quia! Cumque possimus dolor similique eum quasi magni itaque quod maxime corporis dignissimos!
+        </div>
+        <div className="w-1/2">
+
+        </div>
+      </Section>
+    </main>
+  );
 }
