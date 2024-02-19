f47037655f793d7a57b193d333c3a5d1518760c5
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-19 18:31:00 +0100

    create more sections in home

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,52 @@ import Section from "@/components/ui/Section/Section";
 export default function Home() {
   return (
     <main className="mb-20 md:mb-24 lg:mb-32">
-      <Section marginBottom={true} marginTop={true}> 
-
+      {/* Hero */}
+      <Section marginBottom={true} marginTop={true}>
         <div className="w-1/2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, quaerat amet repellat exercitationem illo et consequuntur rem, aliquam quibusdam, excepturi unde suscipit expedita! Perferendis eveniet maiores numquam commodi nostrum dolorem iusto, laudantium aspernatur quae assumenda hic sunt quia! Cumque possimus dolor similique eum quasi magni itaque quod maxime corporis dignissimos!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
+          quaerat amet repellat exercitationem illo et consequuntur rem, aliquam
+          quibusdam, excepturi unde suscipit expedita! Perferendis eveniet
+          maiores numquam commodi nostrum dolorem iusto, laudantium aspernatur
+          quae assumenda hic sunt quia! Cumque possimus dolor similique eum
+          quasi magni itaque quod maxime corporis dignissimos!
         </div>
-        <div className="w-1/2">
+        <div className="w-1/2"></div>
+      </Section>
 
+      {/* Presentation */}
+      <Section marginBottom={true} marginTop={true}>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+          corrupti praesentium dolore assumenda, provident nam, reprehenderit
+          quibusdam a repudiandae autem voluptatum porro cumque nihil possimus
+          ullam soluta? Esse vitae distinctio rerum repellendus. Expedita
+          consequatur unde amet nesciunt placeat omnis atque nisi, id totam
+          veniam porro cupiditate eum, cum quis culpa?
+        </div>
+      </Section>
+
+      {/* Skills */}
+      <Section marginBottom={true} marginTop={true}>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+          corrupti praesentium dolore assumenda, provident nam, reprehenderit
+          quibusdam a repudiandae autem voluptatum porro cumque nihil possimus
+          ullam soluta? Esse vitae distinctio rerum repellendus. Expedita
+          consequatur unde amet nesciunt placeat omnis atque nisi, id totam
+          veniam porro cupiditate eum, cum quis culpa?
+        </div>
+      </Section>
+
+      {/* Contact */}
+      <Section marginBottom={true} marginTop={true}>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+          corrupti praesentium dolore assumenda, provident nam, reprehenderit
+          quibusdam a repudiandae autem voluptatum porro cumque nihil possimus
+          ullam soluta? Esse vitae distinctio rerum repellendus. Expedita
+          consequatur unde amet nesciunt placeat omnis atque nisi, id totam
+          veniam porro cupiditate eum, cum quis culpa?
         </div>
       </Section>
     </main>
