55c3e0997dbff9989e88d60feed293ee3535af9d
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-19 23:28:19 +0100

    create histoire page

---
+++ app/histoire/page.tsx
@@ -0,0 +1,30 @@
+import PageTitle from "@/components/PageTitle/PageTitle";
+import Section from "@/components/Section/Section";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Mon histoire",
+  description:
+    "Pour en savoir plus sur mon histoire",
+};
+
+function Parcours() {
+  return (
+    <main className="mb-20 md:mb-24 lg:mb-32">
+      <PageTitle
+        title="Mon histoire"
+        titleDescription=""
+      />
+      <Section marginBottom={true} marginTop={false} classname="">
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis natus
+          perspiciatis dolorem porro expedita dolores. Natus molestias enim
+          eligendi voluptatibus.
+        </div>
+      </Section>
+    </main>
+  );
+}
+
+export default Parcours;
