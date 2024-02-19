885e17f0584ce3acb7110b1bde62487475d6de62
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-19 18:07:10 +0100

    update header

--- src/features/Header.tsx
+++ src/features/Header.tsx
@@ -4,8 +4,8 @@ import { ThemeToggle } from '../theme/ThemeToggle';
 export function Header() {
   return (
     <header className="">
-      <div className="flex items-center justify-between">
-        <h1 className="">Sébastien Laurent </h1>
+      <div className="flex items-center justify-between px-6 md:px-10 xl:px-16 2xl:max-w-[1280px] 2xl:mx-auto py-4 lg:py-6">
+        <span className="">Sébastien Laurent </span>
         <ThemeToggle />
       </div>
     </header>
