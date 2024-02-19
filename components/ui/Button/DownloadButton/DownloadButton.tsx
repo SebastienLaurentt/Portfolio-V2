b46e34dd56aeb2556b6a368e0e089494625a0bf8
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-19 18:41:37 +0100

    create DownloadButton

---
+++ components/ui/Button/DownloadButton/DownloadButton.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+
+interface DownloadButtonProps {
+  href: string;
+  fileName: string;
+  linkName: string;
+  classname?: string;
+}
+
+const DownloadButton = ({ href, fileName, linkName, classname }: DownloadButtonProps) => {
+  return (
+    <div className={`${classname}`}>
+      <Button>
+        <a href={href} download={fileName}>
+          {linkName}
+        </a>
+      </Button>
+    </div>
+  );
+};
+
+export default DownloadButton;
