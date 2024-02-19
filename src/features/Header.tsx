a7a966b4c4d6d556929263ed3c47aaf177669ab2
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-19 17:40:41 +0100

    reset header css

--- src/features/Header.tsx
+++ src/features/Header.tsx
@@ -3,9 +3,9 @@ import { ThemeToggle } from '../theme/ThemeToggle';
 
 export function Header() {
   return (
-    <header className="border-b border-b-accent fixed top-0 left-0 right-0 bg-background">
-      <div className="container flex items-center py-2 max-w-lg m-auto gap-1">
-        <h1 className="text-2xl font-bold mr-auto">Githread</h1>
+    <header className="">
+      <div className="flex items-center justify-between">
+        <h1 className="">Sébastien Laurent </h1>
         <ThemeToggle />
       </div>
     </header>
