a35b2db6e028bb074bf47735a6e33ba34fb3ab8d
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-20 12:16:09 +0100

    create footer

---
+++ src/features/layout/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export const Footer = () => {
+  return (
+    <footer className="bg-secondary text-white">
+      <div className="flex text-xs md:text-sm items-center px-6 md:px-10 xl:px-16 2xl:max-w-[1280px] 2xl:mx-auto py-8 justify-between items-center ">
+        <span className="">@2024 - Sébastien Laurent</span>
+        <span className="">
+          <Link href="/contact"> Contact </Link>
+        </span>
+      </div>
+    </footer>
+  );
+};
