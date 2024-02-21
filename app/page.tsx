444aeed83b62edddf15e8ce5d55aca371b5e770d
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-21 01:43:18 +0100

    create contact section in home

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,10 @@ import Portfolio from "../public/images/PortfolioV2.png";
 import Stash from "../public/images/Stash.png";
 
 import Next from "../public/images/Next.svg";
-import Wordpress from "../public/images/Wordpress.svg";
-import ReactSvg from "../public/images/React.svg";
 import Node from "../public/images/NodeJs.svg";
+import ReactSvg from "../public/images/React.svg";
 import Tailwind from "../public/images/Tailwind.svg";
+import Wordpress from "../public/images/Wordpress.svg";
 
 const CV = `/pdf/CV.pdf`;
 
@@ -73,7 +73,7 @@ export default function Home() {
         </div>
       </Section>
 
-      {/* Skills */}
+      {/* Projets */}
       <Section marginBottom={true} marginTop={true}>
         <SectionHeader title="Mes dernières" titleHighlight="réalisations" />
         <div className="flex flex-col ">
@@ -110,31 +110,35 @@ export default function Home() {
         </div>
       </Section>
 
-      {/* Contact */}
+      {/* Skills */}
       <Section marginBottom={true} marginTop={true}>
         <SectionHeader title="Des compétences" titleHighlight="transverses" />
         <div className="flex flex-col md:flex-row md:justify-center gap-y-2 md:gap-x-2 ">
           <div className="flex justify-center gap-x-2">
-            <div className="border rounded-xl w-24 h-24 py-4 px-8 flex items-center" >
+            <div className="border rounded-xl w-24 h-24 py-4 px-8 flex items-center">
               <Image src={Next} alt="Logo Next" height={62} width={62} />
             </div>
-            <div className="border rounded-xl w-24 h-24 py-4 px-8 flex items-center" >
+            <div className="border rounded-xl w-24 h-24 py-4 px-8 flex items-center">
               <Image src={Tailwind} alt="Logo Next" height={62} width={62} />
             </div>
-            <div className="border rounded-xl w-24 h-24 py-4 px-8 flex items-center" >
+            <div className="border rounded-xl w-24 h-24 py-4 px-8 flex items-center">
               <Image src={ReactSvg} alt="Logo Next" height={62} width={62} />
             </div>
           </div>
           <div className="flex justify-center gap-x-2">
-            <div className="border rounded-xl w-24 h-24 py-4 px-8 flex items-center" >
+            <div className="border rounded-xl w-24 h-24 py-4 px-8 flex items-center">
               <Image src={Wordpress} alt="Logo Next" height={62} width={62} />
             </div>
-            <div className="border rounded-xl w-24 h-24 py-4 px-8 flex items-center" >
+            <div className="border rounded-xl w-24 h-24 py-4 px-8 flex items-center">
               <Image src={Node} alt="Logo Next" height={62} width={62} />
             </div>
           </div>
-
         </div>
+      </Section>
+
+      {/* Contact */}
+      <Section marginBottom={true} marginTop={true}>
+        <SectionHeader title="Des questions ?" titleHighlight="Contactez-moi !" />
 
       </Section>
     </main>
