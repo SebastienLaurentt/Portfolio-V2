33fc3387b3843d421e1572821318195ca6436cb4
SebastienLaurentt <slaurent.26@gmail.com>
2024-02-29 13:49:08 +0100

    clean home Page and create Contact form

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,16 @@
+import Contact from "@/components/LandingSections/Contact/Contact";
 import Hero from "@/components/LandingSections/Hero/Hero";
 import Presentation from "@/components/LandingSections/Presentation/Presentation";
-
-import Contact from "@/components/LandingSections/Contact/Contact";
 import Projets from "@/components/LandingSections/Projets/Projets";
 import Skills from "@/components/LandingSections/Skills/Skills";
 
 export default function Home() {
   return (
     <main className="mb-20 md:mb-24 lg:mb-32">
-      {/* Hero */}
       <Hero />
-      {/* Presentation */}
       <Presentation />
-
-      {/* Projets */}
       <Projets />
-      {/* Skills */}
       <Skills />
-      {/* Contact */}
       <Contact />
     </main>
   );
