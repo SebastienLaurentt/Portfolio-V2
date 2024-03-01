bf307996329b6346b8e88a712aeee507e1c3134f
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-01 12:34:56 +0100

    remove burgerMenu

--- src/features/layout/Header/Header.tsx
+++ src/features/layout/Header/Header.tsx
@@ -1,47 +1,12 @@
-"use client";
-
-import Link from "next/link";
-import navData from "../../../../data/navData";
-import { ThemeToggle } from "../../../theme/ThemeToggle";
-import { usePathname } from "next/navigation";
-import BurgerMenu from "./BurgerMenu";
-import { useState } from "react";
 import { BrainCircuit } from "lucide-react";
+import { ThemeToggle } from "../../../theme/ThemeToggle";
 
 export function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-  const pathname = usePathname();
-
   return (
     <header className="">
       <div className="flex items-center justify-between px-6 md:px-10 xl:px-16 2xl:max-w-[1400px] xl:mx-auto py-6 lg:py-6">
         <BrainCircuit />
-        <nav className="hidden lg:block">
-          <ul className="flex gap-x-4 border py-3 px-1 rounded-full">
-            {navData.map((link) => (
-              <li key={link.name} className="">
-                <Link
-                  href={link.href}
-                  className={
-                    pathname === link.href ? " py-2 px-4 rounded-full bg-secondary/90" : "py-2 px-4 text-primary/70"
-                  }
-                >
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        
-        <div className="flex">
         <ThemeToggle />
-        <div className="h-8 md:h-10 lg:hidden">
-          <BurgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
-        </div>
-        </div>
-        
-
-
       </div>
     </header>
   );
