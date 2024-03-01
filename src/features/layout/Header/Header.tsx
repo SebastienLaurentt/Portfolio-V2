60fa7ae4236ad4d70df31f00ca5194638f24006a
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-01 12:56:19 +0100

    header icon are now nested in buttons

--- src/features/layout/Header/Header.tsx
+++ src/features/layout/Header/Header.tsx
@@ -1,12 +1,15 @@
 import { BrainCircuit } from "lucide-react";
 import { ThemeToggle } from "../../../theme/ThemeToggle";
+import { Button } from "@/components/ui/button";
 
 export function Header() {
   return (
     <header className="">
       <div className="flex items-center justify-between px-6 md:px-10 xl:px-16 2xl:max-w-[1400px] xl:mx-auto py-6 lg:py-6">
-        <BrainCircuit />
-        <ThemeToggle />
+        <Button variant="ghost"> <BrainCircuit /></Button>
+        <Button variant="ghost"> <ThemeToggle /> </Button>
+        
+        
       </div>
     </header>
   );
