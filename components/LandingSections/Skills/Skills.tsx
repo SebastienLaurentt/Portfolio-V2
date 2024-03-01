d6460ff4bfaf5e4bcd8a466f47b59c61c5869988
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-02 00:24:12 +0100

    update responsive of skills component

--- components/LandingSections/Skills/Skills.tsx
+++ components/LandingSections/Skills/Skills.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Section from "@/components/Section/Section";
 import SectionHeader from "@/components/SectionHeader/SectionHeader";
 import NextJsIcon from "@/components/SvgIcons/NextJsIcon/NextJsIcon";
@@ -6,31 +8,53 @@ import NodeJsIcon from "@/components/SvgIcons/NodeJsIcon/NodeJsIcon";
 import ReactIcon from "@/components/SvgIcons/ReactIcon/ReactIcon";
 import TailwindIcon from "@/components/SvgIcons/Tailwind/Tailwind";
 import WordPressIcon from "@/components/SvgIcons/Wordpress/Wordpress";
+import { useState } from "react";
 
 const Skills = () => {
   const fillColor = "hsl(var(--primary-foreground))";
-  const size = 32;
+  const [size, setSize] = useState(() => getSize());
+
+  // Fonction pour obtenir la taille en fonction de la largeur de la fenêtre
+  function getSize() {
+    const width = window.innerWidth;
+    if (width < 768) {
+      return 28; // Taille plus petite pour les appareils mobiles
+    } else if (width >= 768 && width < 1024) {
+      return 40; // Taille moyenne pour les tablettes
+    } else if (width >= 1024 && width < 1280) {
+      return 52; // Taille plus grande pour les écrans plus larges
+    } else if (width >= 1280 && width < 1400) {
+      return 48; // Taille encore plus grande pour les écrans plus larges
+    } else {
+      return 56; // Taille la plus grande pour les écrans très larges
+    }
+  }
+
+  // Mettre à jour la taille de l'icône lors du redimensionnement de la fenêtre
+  window.addEventListener('resize', () => {
+    setSize(getSize());
+  });
 
   return (
     <Section marginBottom={true} marginTop={true}>
       <SectionHeader title="Des compétences" titleHighlight="transverses" />
-      <div className="flex flex-col md:flex-row md:justify-center gap-y-2 md:gap-x-2 ">
-        <div className="flex justify-center gap-x-2">
-          <div className="border rounded-xl w-24 h-16  justify-center flex items-center">
+      <div className="flex flex-col xl:flex-row xl:justify-center gap-y-2 md:gap-y-3 lg:gap-y-4 xl:gap-x-4">
+        <div className="flex justify-center gap-x-2 md:gap-x-3 lg:gap-x-4">
+          <div className="border rounded-xl w-24 md:w-36 lg:w-48 h-16 md:h-24 lg:h-32  justify-center flex items-center">
             <NextJsIcon fillColor={fillColor} width={size} height={size} />
           </div>
-          <div className="border rounded-xl w-24 h-16 flex justify-center  items-center">
+          <div className="border rounded-xl w-24 md:w-36 lg:w-48 h-16 md:h-24 lg:h-32  flex justify-center  items-center">
             <TailwindIcon fillColor={fillColor} width={size} height={size} />
           </div>
-          <div className="border rounded-xl w-24 h-16 flex justify-center items-center">
+          <div className="border rounded-xl w-24 md:w-36 lg:w-48 h-16 md:h-24 lg:h-32  flex justify-center items-center">
             <ReactIcon fillColor={fillColor} width={size} height={size} />
           </div>
         </div>
-        <div className="flex justify-center gap-x-2">
-          <div className="border rounded-xl w-24 h-16 flex justify-center flex items-center">
+        <div className="flex justify-center gap-x-2 md:gap-x-3 lg:gap-x-4">
+          <div className="border rounded-xl w-24 md:w-36 lg:w-48 h-16 md:h-24 lg:h-32  flex justify-center flex items-center">
             <WordPressIcon fillColor={fillColor} width={size} height={size} />
           </div>
-          <div className="border rounded-xl w-24 h-16 flex justify-center flex items-center">
+          <div className="border rounded-xl w-24 md:w-36 lg:w-48 h-16 md:h-24 lg:h-32  flex justify-center flex items-center">
             <NodeJsIcon fillColor={fillColor} width={size} height={size} />
           </div>
         </div>
