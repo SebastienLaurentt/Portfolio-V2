a29388335b2f31f0407c00c1035fdb1acdb0c9f4
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-01 13:21:45 +0100

    update footer

--- src/features/layout/Footer/Footer.tsx
+++ src/features/layout/Footer/Footer.tsx
@@ -3,11 +3,8 @@ import Link from "next/link";
 export const Footer = () => {
   return (
     <footer className="">
-      <div className="flex text-xs md:text-sm items-center px-6 md:px-10 xl:px-16 2xl:max-w-[1280px] 2xl:mx-auto py-8 justify-between items-center ">
-        <span className="">@2024 - Sébastien Laurent</span>
-        <span className="">
-          <Link href="/contact" className="text-tertiary-foreground"> Contact </Link>
-        </span>
+      <div className="text-center text-xs md:text-sm items-center px-6 md:px-10 xl:px-16 2xl:max-w-[1280px] 2xl:mx-auto py-8">
+        <span className="">&#169; 2024 - Sébastien Laurent</span>
       </div>
     </footer>
   );
