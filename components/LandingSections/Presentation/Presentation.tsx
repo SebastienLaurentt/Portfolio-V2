46396f4af3d590d0efcbe790ac9fd9da01a7f3f8
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-03 23:58:23 +0100

    update Presentation content

--- components/LandingSections/Presentation/Presentation.tsx
+++ components/LandingSections/Presentation/Presentation.tsx
@@ -10,23 +10,7 @@ const Presentation = () => {
         titleHighlight="développement"
       />
 
-      <WordAnimaton paragraph="Après un doctorat en Sciences Cognitives je me suis tourné vers le développement web. Ce choix s'explique par la volonté d'utiliser ma compréhension du cerveau humain dans un secteur qui m'intéresse particulièrement, celui du numérique. Ainsi, de manière autodidacte mais aussi par le biais d'une formation intensive j'explore ce nouvel environnement. J'adore découvrir et apprendre à réaliser de nouvelles fonctionnalités pouvant améliorer l'expérience des utilisateurs." />
-      {/* <div className="text-md md:text-xl lg:text-3xl xl:text-4xl leading-8 md:leading-[48px] lg:leading-[64px] xl:leading-[72px]">
-      Après un doctorat en{" "}
-      <span className="text-tertiary-foreground"> Sciences Cognitives</span>{" "}
-      je me suis tourné vers le développement web. Ce choix s&apos;explique
-      par la volonté d&apos;utiliser ma compréhension du
-      <span className="text-tertiary-foreground"> cerveau humain</span> dans
-      un secteur qui m&apos;intéresse particulièrement, celui du{" "}
-      <span className="text-tertiary-foreground"> numérique</span>. Ainsi,
-      de manière autodidacte mais aussi par le biais d&apos;une formation
-      intensive j&apos;explore ce nouvel environnement. J&apos;adore
-      découvrir et apprendre à réaliser de{" "}
-      <span className="text-tertiary-foreground">
-        nouvelles fonctionnalités
-      </span>{" "}
-      pouvant améliorer l&apos;expérience des utilisateurs.
-    </div> */}
+      <WordAnimaton paragraph="Je suis un développeur web qui vise à intégrer les connaissances sur le cerveau que j'ai apprise durant mes expériences dans la recherche. Mon objectif est de trouver l'équilibre entre une interface attractive et une expérience utilisateur fluide. " />
     </Section>
   );
 };
