be8132a89f8b30146b2fb9e9d72fe58b6473dd31
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-03 21:10:34 +0100

    update wordAnimation component

--- components/Animation/WordAnimation.tsx
+++ components/Animation/WordAnimation.tsx
@@ -1,38 +1,45 @@
-import { motion, useScroll, useTransform } from 'framer-motion';
-import React, { useRef } from 'react';
-import styles from './style.module.scss';
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
-export default function Paragraph({paragraph}: { paragraph: string }) {
-
+export default function Paragraph({ paragraph }: { paragraph: string }) {
   const container = useRef(null);
   const { scrollYProgress } = useScroll({
     target: container,
-    offset: ["start 0.9", "start 0.25"]
-  })
+    offset: ["start 0.9", "start 0.25"],
+  });
 
-  const words = paragraph.split(" ")
+  const words = paragraph.split(" ");
   return (
-    <p 
-      ref={container}         
-      className={styles.paragraph}
-    >
-    {
-      words.map( (word: string, i: number) => {
-        const start = i / words.length
-        const end = start + (1 / words.length)
-        return <Word key={i} progress={scrollYProgress} range={[start, end]}>{word}</Word>
-      })
-    }
+    <p ref={container} className="paragraph">
+      {words.map((word: string, i: number) => {
+        const start = i / words.length;
+        const end = start + 1 / words.length;
+        return (
+          <Word key={i} progress={scrollYProgress} range={[start, end]}>
+            {word}
+          </Word>
+        );
+      })}
     </p>
-  )
+  );
 }
 
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
-const Word = ({children, progress, range}: { children: ReactNode, progress: any, range: any }) => {
-  const opacity = useTransform(progress, range, [0, 1])
-  return <span className={styles.word}>
-    <span className={styles.shadow}>{children}</span>
-    <motion.span style={{opacity: opacity}}>{children}</motion.span>
-  </span>
-}
+const Word = ({
+  children,
+  progress,
+  range,
+}: {
+  children: ReactNode;
+  progress: any;
+  range: any;
+}) => {
+  const opacity = useTransform(progress, range, [0, 1]);
+  return (
+    <span className="word">
+      <span className="wordShadow">{children}</span>
+      <motion.span style={{ opacity: opacity }}>{children}</motion.span>
+    </span>
+  );
+};
