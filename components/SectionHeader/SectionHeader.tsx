e3a5fe87452ccc3d5bf6f70ca57d2b9f6f140322
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-04 15:55:01 +0100

    remove titleDescription props in section Header

--- components/SectionHeader/SectionHeader.tsx
+++ components/SectionHeader/SectionHeader.tsx
@@ -2,21 +2,16 @@ import React from "react";
 
 interface SectionHeaderProps {
   title: string;
-  titleDescription?: string;
 }
 
 const SectionHeader: React.FC<SectionHeaderProps> = ({
   title,
-  titleDescription,
 }) => {
   return (
     <div className="mb-20 md:mb-28 lg:mb-36 xl:mb-48 text-center">
       <h2>
         {title} 
       </h2>
-      <p className="sectionHeaderDescription">
-        {titleDescription}
-      </p>
     </div>
   );
 };
