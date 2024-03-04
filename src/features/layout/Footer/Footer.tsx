c9febc2938eadcaebec070438952a95c381d14d4
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-04 14:59:17 +0100

    update footer

--- src/features/layout/Footer/Footer.tsx
+++ src/features/layout/Footer/Footer.tsx
@@ -9,16 +9,16 @@ export const Footer = () => {
 
   return (
     <footer className="">
-      <div className="text-center text-xs md:text-sm items-center px-6 md:px-10 xl:px-16 2xl:max-w-[1280px] 2xl:mx-auto py-8">
+      <div className="text-center text-xs md:text-sm items-center px-6 md:px-10 xl:px-16 w-[210px] md:w-[400px] lg:w-[600px] mx-auto py-6  border-t">
         <span className="">&#169;2024 - Sébastien Laurent</span>
         <ul className="flex justify-center mt-2 gap-x-2">
-          <li className="border rounded-full w-12 h-12 flex flex-col justify-center  items-center">
+          <li className="border rounded-full w-8 h-8 flex flex-col justify-center  items-center">
             <XIcon fillColor={fillColor} />
           </li>
-          <li className="border rounded-full w-12 h-12  flex flex-col justify-center  items-center">
+          <li className="border rounded-full w-8 h-8  flex flex-col justify-center  items-center">
             <LinkedInIcon fillColor={fillColor}/>
           </li>
-          <li className="border rounded-full w-12 h-12  flex flex-col justify-center  items-center">
+          <li className="border rounded-full w-8 h-8  flex flex-col justify-center  items-center">
             <GitHubIcon fillColor={fillColor} />
           </li>
         </ul>
