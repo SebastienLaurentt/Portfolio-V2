438888679db479f7b47b45f606483897da3f269e
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-04 01:42:53 +0100

    update footer content

--- src/features/layout/Footer/Footer.tsx
+++ src/features/layout/Footer/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer className="">
       <div className="text-center text-xs md:text-sm items-center px-6 md:px-10 xl:px-16 2xl:max-w-[1280px] 2xl:mx-auto py-8">
-        <span className="">&#169; 2024 - Sébastien Laurent</span>
+        <span className="">&#169;2024 - Sébastien Laurent</span>
       </div>
     </footer>
   );
