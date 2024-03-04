e6d5123d1f27d11a93e5a06d6a98944ed0d2a2ce
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-04 14:25:35 +0100

    import social icon on footer

--- src/features/layout/Footer/Footer.tsx
+++ src/features/layout/Footer/Footer.tsx
@@ -1,10 +1,23 @@
-import Link from "next/link";
+import GitHubIcon from "@/components/SvgIcons/GitHubIcon/GitHubIcon";
+import XIcon from "@/components/SvgIcons/XIcon/XIcon";
+import { LinkedInLogoIcon } from "@radix-ui/react-icons";
 
 export const Footer = () => {
   return (
     <footer className="">
       <div className="text-center text-xs md:text-sm items-center px-6 md:px-10 xl:px-16 2xl:max-w-[1280px] 2xl:mx-auto py-8">
         <span className="">&#169;2024 - Sébastien Laurent</span>
+        <ul className="flex justify-center mt-8 gap-x-2">
+          <li>
+            <XIcon />
+          </li>
+          <li>
+            <LinkedInLogoIcon />
+          </li>
+          <li>
+            <GitHubIcon />
+          </li>
+        </ul>
       </div>
     </footer>
   );
