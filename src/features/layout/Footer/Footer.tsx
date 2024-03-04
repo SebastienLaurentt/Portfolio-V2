9bf2bb033010e6db44fa5401388eeff86421dc97
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-04 15:28:02 +0100

    create customClass in globals for socialIcon footer

--- src/features/layout/Footer/Footer.tsx
+++ src/features/layout/Footer/Footer.tsx
@@ -12,13 +12,13 @@ export const Footer = () => {
       <div className="text-center text-xs md:text-sm items-center px-6 md:px-10 xl:px-16 w-[210px] md:w-[400px] lg:w-[600px] mx-auto py-6  border-t">
         <span className="">&#169;2024 - Sébastien Laurent</span>
         <ul className="flex justify-center mt-2 gap-x-2">
-          <li className="border rounded-full w-8 h-8 flex flex-col justify-center  items-center">
+          <li className="socialIcon">
             <XIcon fillColor={fillColor} />
           </li>
-          <li className="border rounded-full w-8 h-8  flex flex-col justify-center  items-center">
+          <li className="socialIcon">
             <LinkedInIcon fillColor={fillColor}/>
           </li>
-          <li className="border rounded-full w-8 h-8  flex flex-col justify-center  items-center">
+          <li className="socialIcon">
             <GitHubIcon fillColor={fillColor} />
           </li>
         </ul>
