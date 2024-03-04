2806cc24eabccc86536b5af64a7dc52c780e9ffb
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-04 02:05:40 +0100

    update projectCard for lg display

--- components/ProjectCard/ProjectCard.tsx
+++ components/ProjectCard/ProjectCard.tsx
@@ -25,7 +25,7 @@ const ProjectCard = ({
       <div className={`md:flex ${isRowReverse} md:items-center md:gap-x-12 w-full`}>
         <div className="text-center md:text-left  md:w-4/5 xl:w-3/5 ">
           <h3 className="mb-4 ">{projectTitle}</h3>
-          <p className="max-w-[320px]  mx-auto ">{projectDescription}</p>
+          <p className="max-w-[320px] lg:max-w-[300px] mx-auto md:mx-0">{projectDescription}</p>
         </div>
         <div className="mt-[40px] md:mt-0 ">
           <Image src={src} alt={alt} className="rounded-lg" />
