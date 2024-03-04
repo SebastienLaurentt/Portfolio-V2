70e21e9e6ad272118dc40b13db9dea52ee51c0f2
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-04 17:23:03 +0100

    update project card css

--- components/ProjectCard/ProjectCard.tsx
+++ components/ProjectCard/ProjectCard.tsx
@@ -25,10 +25,10 @@ const ProjectCard = ({
   
   return (
     <div className={`${isMarginBottom} ${classname}`}>
-      <div className={`md:flex ${isRowReverse} md:items-center md:gap-x-12 xl:gap-x-20 w-full`}>
-        <div className="text-center md:text-left  md:w-4/5 xl:w-3/5 ">
+      <div className={`md:flex ${isRowReverse} md:items-center md:gap-x-12 lg:gap-x-16 xl:gap-x-28 w-full`}>
+        <div className="text-center md:text-left  max-w-[320px] md:max-w-[300px] mx-auto">
           <h3 className="mb-2 md:mb-4 ">{projectTitle}</h3>
-          <p className="max-w-[320px] lg:max-w-[300px] mx-auto md:mx-0">{projectDescription}</p>
+          <p className=" md:mx-0">{projectDescription}</p>
         </div>
         <div className="mt-[20px] md:mt-0 ">
           <Image src={src} alt={alt} className="rounded-lg" />
