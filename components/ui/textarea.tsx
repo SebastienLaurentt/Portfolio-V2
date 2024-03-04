04dbc178aa5af4f742c2819d51b79a5942c4de8c
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-05 00:56:02 +0100

    delete focus visible on input and textarea

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "text-tertiary-foreground flex min-h-[60px] w-full  border-b border-input bg-transparent px-3 py-2 text-sm  placeholder:text-tertiary-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          "text-tertiary-foreground flex min-h-[60px] w-full  border-b border-input bg-transparent px-3 py-2 text-sm  placeholder:text-tertiary-foreground focus-visible:outline-none  focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
