01151090752a9f80164fa4a5a5e6ee35cb608a98
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-04 01:01:23 +0100

    remove titleHighlight in SectionHeader component

--- components/SectionHeader/SectionHeader.tsx
+++ components/SectionHeader/SectionHeader.tsx
@@ -2,19 +2,17 @@ import React from "react";
 
 interface SectionHeaderProps {
   title: string;
-  titleHighlight?: string;
   titleDescription?: string;
 }
 
 const SectionHeader: React.FC<SectionHeaderProps> = ({
   title,
-  titleHighlight,
   titleDescription,
 }) => {
   return (
     <div className="mb-12 md:mb-24 text-center">
       <h2>
-        {title} <span className="text-tertiary-foreground">{titleHighlight}</span>
+        {title} 
       </h2>
       <p className="sectionHeaderDescription">
         {titleDescription}
