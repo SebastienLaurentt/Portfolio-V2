de26a13cc110da5485cc61d58f9f26ce8dadd1d9
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-04 14:26:37 +0100

    update social list css on footer

--- src/features/layout/Footer/Footer.tsx
+++ src/features/layout/Footer/Footer.tsx
@@ -8,13 +8,13 @@ export const Footer = () => {
       <div className="text-center text-xs md:text-sm items-center px-6 md:px-10 xl:px-16 2xl:max-w-[1280px] 2xl:mx-auto py-8">
         <span className="">&#169;2024 - Sébastien Laurent</span>
         <ul className="flex justify-center mt-8 gap-x-2">
-          <li>
+          <li className="border rounded-xl w-24 md:w-36 lg:w-48 h-16 md:h-24 lg:h-32 flex flex-col justify-center  items-center">
             <XIcon />
           </li>
-          <li>
+          <li className="border rounded-xl w-24 md:w-36 lg:w-48 h-16 md:h-24 lg:h-32 flex flex-col justify-center  items-center">
             <LinkedInLogoIcon />
           </li>
-          <li>
+          <li className="border rounded-xl w-24 md:w-36 lg:w-48 h-16 md:h-24 lg:h-32 flex flex-col justify-center  items-center">
             <GitHubIcon />
           </li>
         </ul>
