24cefd5b9aa88660ce10cc25e5ebbff1c5151254
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-04 14:43:23 +0100

    update social icon css  on footer

--- components/SvgIcons/XIcon/XIcon.tsx
+++ components/SvgIcons/XIcon/XIcon.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+interface XIconProps {
+  fillColor: string;
+}
 
-const XIcon = () => {
+const XIcon = ({ fillColor }: XIconProps) => {
   return (
     <svg
-      width="300"
-      height="271"
+      style={{ width: "auto", height: "50%" }}
+      viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
+      fill={fillColor}
     >
       <path d="m236 0h46l-101 115 118 156h-92.6l-72.5-94.8-83 94.8h-46l107-123-113-148h94.9l65.5 86.6zm-16.1 244h25.5l-165-218h-27.4z" />
     </svg>
--- src/features/layout/Footer/Footer.tsx
+++ src/features/layout/Footer/Footer.tsx
@@ -1,21 +1,25 @@
 import GitHubIcon from "@/components/SvgIcons/GitHubIcon/GitHubIcon";
+import LinkedInIcon from "@/components/SvgIcons/LinkedInIcon/LinkedInIcon";
 import XIcon from "@/components/SvgIcons/XIcon/XIcon";
-import { LinkedInLogoIcon } from "@radix-ui/react-icons";
+
 
 export const Footer = () => {
+
+  const fillColor = "hsl(var(--primary-foreground))";
+
   return (
     <footer className="">
       <div className="text-center text-xs md:text-sm items-center px-6 md:px-10 xl:px-16 2xl:max-w-[1280px] 2xl:mx-auto py-8">
         <span className="">&#169;2024 - Sébastien Laurent</span>
-        <ul className="flex justify-center mt-8 gap-x-2">
-          <li className="border rounded-xl w-24 md:w-36 lg:w-48 h-16 md:h-24 lg:h-32 flex flex-col justify-center  items-center">
-            <XIcon />
+        <ul className="flex justify-center mt-2 gap-x-2">
+          <li className="border rounded-full w-12 h-12 flex flex-col justify-center  items-center">
+            <XIcon fillColor={fillColor} />
           </li>
-          <li className="border rounded-xl w-24 md:w-36 lg:w-48 h-16 md:h-24 lg:h-32 flex flex-col justify-center  items-center">
-            <LinkedInLogoIcon />
+          <li className="border rounded-full w-12 h-12  flex flex-col justify-center  items-center">
+            <LinkedInIcon fillColor={fillColor}/>
           </li>
-          <li className="border rounded-xl w-24 md:w-36 lg:w-48 h-16 md:h-24 lg:h-32 flex flex-col justify-center  items-center">
-            <GitHubIcon />
+          <li className="border rounded-full w-12 h-12  flex flex-col justify-center  items-center">
+            <GitHubIcon fillColor={fillColor} />
           </li>
         </ul>
       </div>
