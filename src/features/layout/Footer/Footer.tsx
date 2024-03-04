0f453c9cce0c07d40f6e8e9d0ef4a8a9e4fbd6d0
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-04 15:37:19 +0100

    li socialIcon are nested with corresponding link

--- src/features/layout/Footer/Footer.tsx
+++ src/features/layout/Footer/Footer.tsx
@@ -1,10 +1,9 @@
 import GitHubIcon from "@/components/SvgIcons/GitHubIcon/GitHubIcon";
 import LinkedInIcon from "@/components/SvgIcons/LinkedInIcon/LinkedInIcon";
 import XIcon from "@/components/SvgIcons/XIcon/XIcon";
-
+import Link from "next/link";
 
 export const Footer = () => {
-
   const fillColor = "hsl(var(--primary-foreground))";
 
   return (
@@ -12,15 +11,24 @@ export const Footer = () => {
       <div className="text-center text-xs md:text-sm items-center px-6 md:px-10 xl:px-16 w-[210px] md:w-[400px] lg:w-[600px] mx-auto py-6  border-t">
         <span className="">&#169;2024 - Sébastien Laurent</span>
         <ul className="flex justify-center mt-2 gap-x-2">
-          <li className="socialIcon">
-            <XIcon fillColor={fillColor} />
-          </li>
-          <li className="socialIcon">
-            <LinkedInIcon fillColor={fillColor}/>
-          </li>
-          <li className="socialIcon">
-            <GitHubIcon fillColor={fillColor} />
-          </li>
+          <Link href="https://twitter.com/Seb_webdev" target="_blank">
+            <li className="socialIcon">
+              <XIcon fillColor={fillColor} />
+            </li>
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/s%C3%A9bastien-laurent-3a007328a/"
+            target="_blank"
+          >
+            <li className="socialIcon">
+              <LinkedInIcon fillColor={fillColor} />
+            </li>
+          </Link>
+          <Link href="https://github.com/SebastienLaurentt" target="_blank">
+            <li className="socialIcon">
+              <GitHubIcon fillColor={fillColor} />
+            </li>
+          </Link>
         </ul>
       </div>
     </footer>
