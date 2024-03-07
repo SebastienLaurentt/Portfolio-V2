09c94d936163c0dc296c9bb20b37482b18181b7b
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-07 01:08:30 +0100

    import typeScript Icon in skills section

--- components/LandingSections/Skills/Skills.tsx
+++ components/LandingSections/Skills/Skills.tsx
@@ -6,6 +6,7 @@ import NextJsIcon from "@/components/SvgIcons/NextJsIcon/NextJsIcon";
 import NodeJsIcon from "@/components/SvgIcons/NodeJsIcon/NodeJsIcon";
 import ReactIcon from "@/components/SvgIcons/ReactIcon/ReactIcon";
 import TailwindIcon from "@/components/SvgIcons/Tailwind/Tailwind";
+import TypeScriptIcon from "@/components/SvgIcons/TypeScriptIcon/TypeScriptIcon";
 import WordPressIcon from "@/components/SvgIcons/Wordpress/Wordpress";
 import { useEffect, useRef, useState } from "react";
 
@@ -43,6 +44,18 @@ const Skills = () => {
         ref={sectionRef}
         className="flex flex-col xl:flex-row xl:justify-center gap-y-2 md:gap-y-3 lg:gap-y-4 xl:gap-x-4"
       >
+        <div className="flex justify-center">
+        <div
+          className={`border rounded-xl w-24 md:w-36 lg:w-48 h-16 md:h-24 lg:h-32 flex flex-col justify-center items-center ${
+            isVisible
+              ? "opacity-100 transition-opacity duration-200 "
+              : "opacity-0 transition-opacity"
+          }`}
+        >
+          <NextJsIcon fillColor={fillColor} />
+        </div>
+        </div>
+
         <div className="flex justify-center gap-x-2 md:gap-x-3 lg:gap-x-4">
           <div
             className={`border rounded-xl w-24 md:w-36 lg:w-48 h-16 md:h-24 lg:h-32 flex flex-col justify-center items-center ${
@@ -51,7 +64,7 @@ const Skills = () => {
                 : "opacity-0 transition-opacity"
             }`}
           >
-            <NextJsIcon fillColor={fillColor} />
+            <TypeScriptIcon fillColor={fillColor} />
           </div>
           <div
             className={`border rounded-xl w-24 md:w-36 lg:w-48 h-16 md:h-24 lg:h-32  flex justify-center items-center ${
