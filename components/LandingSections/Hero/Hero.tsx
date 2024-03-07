d1a54b0cbf514ad04ed644b1b677419eee47c938
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-07 01:40:46 +0100

    update hero section  for responsive

--- components/LandingSections/Hero/Hero.tsx
+++ components/LandingSections/Hero/Hero.tsx
@@ -20,19 +20,19 @@ const Hero = () => {
         <h1 >
           Développeur Web
         </h1>
-        <span className="text-center lg:text-left text-lg lg:text-xl xl:text-2xl">Bienvenue sur mon portfolio !</span>
+        <span className="text-center xl:text-left text-lg lg:text-xl xl:text-2xl">Bienvenue sur mon portfolio !</span>
         <DownloadButton
           href={CV}
           fileName="Sebastien_Laurent_CV.pdf"
           linkName="Télécharger CV"
-          classname="mt-4 flex justify-center lg:justify-start z-10"
+          classname="mt-4 flex justify-center xl:justify-start z-10"
         />
       </div>
       <div className="">
         <Image
           src={HeroImg}
           alt="Logo Galopins"
-          className="mt-16 md:mt-20 lg:mt-4 md:w-3/4 lg:w-2/3 xl:w-3/4 md:mx-auto xl:mr-0 "
+          className="mt-16 md:mt-20 lg:mt-20 xl:mt-0 md:w-3/4 lg:w-2/3 xl:w-3/4 md:mx-auto xl:mr-0 "
         />
       </div>
       <BackgroundBeams />
