bceb91ae44f43a2ec17a80f06250a1c275b2b131
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-07 17:45:31 +0100

    set lang to fr

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <>
-      <html lang="en" suppressHydrationWarning>
+      <html lang="fr" suppressHydrationWarning>
         <head />
         <body className={roboto.className}>
           <ThemeProvider
