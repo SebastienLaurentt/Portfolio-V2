bbe15378ec1d90e0c9d5d3d72cb191cdb00693fc
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-07 20:24:04 +0100

    update footer socialIcons list to improve accessibility

--- src/features/layout/Footer/Footer.tsx
+++ src/features/layout/Footer/Footer.tsx
@@ -10,25 +10,38 @@ export const Footer = () => {
     <footer className="">
       <div className="text-center text-xs md:text-sm items-center px-6 md:px-10 xl:px-16 w-[210px] md:w-[400px] lg:w-[600px] mx-auto py-6  border-t">
         <span className="">&#169;2024 - Sébastien Laurent</span>
+
+        {/* Social Icons list */}
         <ul className="flex justify-center mt-2 gap-x-2">
-          <Link href="https://twitter.com/Seb_webdev" target="_blank">
-            <li className="socialIcon">
+          <li>
+            <Link
+              href="https://twitter.com/Seb_webdev"
+              target="_blank"
+              className="socialIcon"
+            >
               <XIcon fillColor={fillColor} />
-            </li>
-          </Link>
-          <Link
-            href="https://www.linkedin.com/in/s%C3%A9bastien-laurent-3a007328a/"
-            target="_blank"
-          >
-            <li className="socialIcon">
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="https://www.linkedin.com/in/s%C3%A9bastien-laurent-3a007328a/"
+              target="_blank"
+              className="socialIcon"
+            >
               <LinkedInIcon fillColor={fillColor} />
-            </li>
-          </Link>
-          <Link href="https://github.com/SebastienLaurentt" target="_blank">
-            <li className="socialIcon">
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              href="https://github.com/SebastienLaurentt"
+              target="_blank"
+              className="socialIcon"
+            >
               <GitHubIcon fillColor={fillColor} />
-            </li>
-          </Link>
+            </Link>
+          </li>
         </ul>
       </div>
     </footer>
