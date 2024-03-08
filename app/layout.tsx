ce9baea4d0de665a6d4d9fd4c6f5c47cdbb39ecc
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-08 02:06:15 +0100

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,21 @@
+import ScrollToTopButton from "@/components/Button/DownloadButton/ScrollToTopButton/ScrollToTopButton";
 import { Footer } from "@/src/features/layout/Footer/Footer";
+import { Roboto } from "next/font/google";
 import { Header } from "../src/features/layout/Header/Header";
 import { ThemeProvider } from "../src/theme/ThemeProvider";
-import './globals.css';
-import ScrollToTopButton from "@/components/Button/DownloadButton/ScrollToTopButton/ScrollToTopButton";
-import { Roboto } from 'next/font/google'
+import "./globals.css";
 
 export const metadata = {
-  title: 'Sébastien Laurent - Développeur Web',
-  description: "Bienvenue sur mon portfolio ! Venez découvrir mon histoire et les derniers projets que j'ai réalisés."
-}
+  title: "Sébastien Laurent - Développeur Web",
+  description:
+    "Bienvenue sur mon portfolio ! Venez découvrir mon histoire et les derniers projets que j'ai réalisés.",
+};
 
 const roboto = Roboto({
-  weight: '400', // if single weight, otherwise you use array like [400, 500, 700],
-  style: 'normal', // if single style, otherwise you use array like ['normal', 'italic']
-  subsets: ['latin'],
-})
+  weight: "400", // if single weight, otherwise you use array like [400, 500, 700],
+  style: "normal", // if single style, otherwise you use array like ['normal', 'italic']
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -24,13 +25,11 @@ export default function RootLayout({
   return (
     <>
       <html lang="fr" suppressHydrationWarning>
-        <head />
+        <head>
+          <link rel="icon" href="/favicon.png" sizes="any" />
+        </head>
         <body className={roboto.className}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-          >
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
             <div className=" flex flex-col h-full">
               <Header />
               <div>{children}</div>
