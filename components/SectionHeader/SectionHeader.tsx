02ce6981a5bcee93ec3885c3d4eef0f8856a47f0
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-08 02:06:25 +0200

    update span number in section header

--- components/SectionHeader/SectionHeader.tsx
+++ components/SectionHeader/SectionHeader.tsx
@@ -7,8 +7,8 @@ interface SectionHeaderProps {
 
 const SectionHeader: React.FC<SectionHeaderProps> = ({ number, title }) => {
   return (
-    <div className="mb-20 flex flex-row items-center justify-center text-center md:mb-28 lg:mb-36 xl:mb-44">
-      <span>({number})</span>
+    <div className="mb-20 flex flex-row items-end justify-center text-center md:mb-28 lg:mb-36 xl:mb-44 ">
+      <span className="mr-2 text-primary-foreground">[{number}.]</span>
       <h2>{title}</h2>
     </div>
   );
