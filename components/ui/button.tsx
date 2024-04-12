efbc21673d8e52c1eae80a911743959a827df50b
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-12 21:35:42 +0200

    update css

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,12 +5,12 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-xs font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm lg:text-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default:
-          "bg-secondary hover:bg-primary  text-secondary-foreground hover:text-tertiary-foreground  border hover:border-tertiary-foreground font-semibold rounded-xl",
+          "bg-secondary hover:bg-primary  text-secondary-foreground hover:text-tertiary-foreground  border hover:border-tertiary-foreground font-medium rounded-xl",
         icons:
           "bg-background  text-tertiary-foreground border border-b-2 hover:border-tertiary-foreground hover:border rounded-lg ",
         links:
@@ -21,7 +21,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-6 py-5",
+        default: "h-10 px-6 py-4",
         links: "py-1 px-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
