21eb24a13793651fffad2c4486423b82c51fe5b2
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-15 00:08:24 +0200

    remove number props

--- components/SectionHeader/SectionHeader.tsx
+++ components/SectionHeader/SectionHeader.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 
 interface SectionHeaderProps {
-  number?: string;
   title: string;
 }
 
-const SectionHeader: React.FC<SectionHeaderProps> = ({ number, title }) => {
+const SectionHeader: React.FC<SectionHeaderProps> = ({  title }) => {
   return (
     <div className="mb-20 flex justify-center text-center md:mb-28 lg:mb-36 xl:mb-44 ">
-  
       <h2>{title}</h2>
     </div>
   );
