85216b6c3fc0aa96d4f88b672139e4e059038b65
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-17 14:52:14 +0200

    remove accent

--- components/Animation/ParagraphAnimation.tsx
+++ components/Animation/ParagraphAnimation.tsx
@@ -18,7 +18,7 @@ export default function ParagraphAnimation() {
       className="mx-auto flex max-w-[320px] flex-wrap text-lg leading-9 text-tertiary-foreground md:max-w-[580px] md:text-2xl md:leading-[48px] lg:max-w-[800px] lg:text-4xl lg:leading-[68px] xl:max-w-[1200px] xl:text-5xl xl:leading-[72px]"
       style={{ opacity }}
     >
-Originaire du domaine des neurosciences, mon but est aujourd&apos;hui  d&apos;utiliser les connaissances acquises sur le fonctionnement cerebral pour créer des applications web offrant une experience utilisateur fluide et intuitive.
+Originaire du domaine des neurosciences, mon but est aujourd&apos;hui  d&apos;utiliser les connaissances acquises sur le fonctionnement cerebral pour creer des applications web offrant une experience utilisateur fluide et intuitive.
     </motion.p>
   );
 }
