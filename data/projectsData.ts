8f5e19b80e01b5929d80151288f74923b03dd3e5
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-20 01:19:16 +0200

    update data

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,21 +1,21 @@
 const projectsData = {
   stash: {
-    nom: "Projet 1",
-    description: "Description du projet 1",
+    name: "Stash",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab odio id doloremque sunt optio, ex officia perspiciatis voluptate corrupti necessitatibus.",
     date: "2022-03-15",
     tech: ["React", "Next.js", "Tailwind CSS"],
     images: ["image1.jpg", "image2.jpg", "image3.jpg"],
   },
   sandamal: {
-    nom: "Projet 2",
-    description: "Description du projet 2",
+    name: "Sandamal",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab odio id doloremque sunt optio, ex officia perspiciatis voluptate corrupti necessitatibus.",
     date: "2022-05-20",
     tech: ["Vue.js", "Express", "MongoDB"],
     images: ["image4.jpg", "image5.jpg", "image6.jpg"],
   },
   galopins: {
-    nom: "Projet 3",
-    description: "Description du projet 3",
+    name: "Galopins",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab odio id doloremque sunt optio, ex officia perspiciatis voluptate corrupti necessitatibus.",
     date: "2022-08-10",
     tech: ["Angular", "Node.js", "PostgreSQL"],
     images: ["image7.jpg", "image8.jpg", "image9.jpg"],
