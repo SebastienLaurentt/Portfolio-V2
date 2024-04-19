e05ad0ffb298feba09280d460ae85832bd8b3fe4
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-20 01:22:44 +0200

    update dataFetch

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -4,7 +4,7 @@ import projectsData from "@/data/projectsData";
 export default function Page({ params }: { params: { slug: string } }) {
   const slug = params.slug;
   const project = projectsData[slug as keyof typeof projectsData] as {
-    nom: string;
+    name: string;
     description: string;
     date: string;
     tech: string[];
@@ -13,9 +13,9 @@ export default function Page({ params }: { params: { slug: string } }) {
   return (
     <Section classname="my-16 lg:my-20 flex flex-col xl:flex-row ">
       <div className="w-1/3">
-        <h2 className="text-center">{project.description}</h2>
-        <span>{project.date}</span>
+        <h2 className="text-left">{project.name}</h2>
         <p>{project.description}</p>
+        <span>{project.date}</span>
         <ul>
           {project.tech.map((tech) => (
             <li key={tech}>{tech}</li>
