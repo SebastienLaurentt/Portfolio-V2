99db9c067b58f3df8d73a6f5c1137575e3c89c2c
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-20 01:15:34 +0200

    link content from projectData and start design

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,23 +1,29 @@
-
-
 import Section from "@/components/Section/Section";
 import projectsData from "@/data/projectsData";
 
-export default function Page({
-  params,
-}: {
-  params: { slug: string };
-}) {
+export default function Page({ params }: { params: { slug: string } }) {
   const slug = params.slug;
-  const project = projectsData[slug as keyof typeof projectsData] as { nom: string; description: string; date: string; tech: string[]; images: string[]; };
+  const project = projectsData[slug as keyof typeof projectsData] as {
+    nom: string;
+    description: string;
+    date: string;
+    tech: string[];
+    images: string[];
+  };
   return (
-    <Section classname="my-16 lg:my-20">
-      <div>
-        {project ? (
-          <h2>{project.description}</h2>
-        ) : (
-          <p>Aucune description disponible pour ce projet</p>
-        )}
+    <Section classname="my-16 lg:my-20 flex flex-col xl:flex-row ">
+      <div className="w-1/3">
+        <h2 className="text-center">{project.description}</h2>
+        <span>{project.date}</span>
+        <p>{project.description}</p>
+        <ul>
+          {project.tech.map((tech) => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="w-1/3">
+        
       </div>
     </Section>
   );
