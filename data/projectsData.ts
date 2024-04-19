a9c29072949d376d576c998cee2116a3c1e20824
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-20 01:25:59 +0200

    import img

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,24 +1,26 @@
+import SandamalLarge from "../public/images/SandamalLarge.png";
+
 const projectsData = {
   stash: {
     name: "Stash",
     description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab odio id doloremque sunt optio, ex officia perspiciatis voluptate corrupti necessitatibus.",
     date: "2022-03-15",
     tech: ["React", "Next.js", "Tailwind CSS"],
-    images: ["image1.jpg", "image2.jpg", "image3.jpg"],
+    images: [SandamalLarge, SandamalLarge, SandamalLarge],
   },
   sandamal: {
     name: "Sandamal",
     description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab odio id doloremque sunt optio, ex officia perspiciatis voluptate corrupti necessitatibus.",
     date: "2022-05-20",
     tech: ["Vue.js", "Express", "MongoDB"],
-    images: ["image4.jpg", "image5.jpg", "image6.jpg"],
+    images: [SandamalLarge, SandamalLarge, SandamalLarge],
   },
   galopins: {
     name: "Galopins",
     description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab odio id doloremque sunt optio, ex officia perspiciatis voluptate corrupti necessitatibus.",
     date: "2022-08-10",
     tech: ["Angular", "Node.js", "PostgreSQL"],
-    images: ["image7.jpg", "image8.jpg", "image9.jpg"],
+    images: [SandamalLarge, SandamalLarge, SandamalLarge],
   },
 };
 
