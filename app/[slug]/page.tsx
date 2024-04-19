c59208a89e1927e8ce754734c5aa5dd14de6b661
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-20 00:53:46 +0200

    update projectData and slugPage to allow projectData file to be fetch in slug page

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,12 +1,24 @@
-import Section from '@/components/Section/Section'
-import React from 'react'
 
-const page = () => {
+
+import Section from "@/components/Section/Section";
+import projectsData from "@/data/projectsData";
+
+export default function Page({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const slug = params.slug;
+  const project = projectsData[slug as keyof typeof projectsData] as { nom: string; description: string; date: string; tech: string[]; images: string[]; };
   return (
     <Section>
-      <p>div</p>
+      <div>
+        {project ? (
+          <p>{project.description}</p>
+        ) : (
+          <p>Aucune description disponible pour ce projet</p>
+        )}
+      </div>
     </Section>
-  )
+  );
 }
-
-export default page
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,26 +1,25 @@
-const projectsData = [
-  {
-    id: "projet1",
+const projectsData = {
+  stash: {
     nom: "Projet 1",
     description: "Description du projet 1",
     date: "2022-03-15",
     tech: ["React", "Next.js", "Tailwind CSS"],
     images: ["image1.jpg", "image2.jpg", "image3.jpg"],
   },
-  {
-    id: "projet2",
+  sandamal: {
     nom: "Projet 2",
     description: "Description du projet 2",
     date: "2022-05-20",
     tech: ["Vue.js", "Express", "MongoDB"],
     images: ["image4.jpg", "image5.jpg", "image6.jpg"],
   },
-  {
-    id: "projet3",
+  galopins: {
     nom: "Projet 3",
     description: "Description du projet 3",
     date: "2022-08-10",
     tech: ["Angular", "Node.js", "PostgreSQL"],
     images: ["image7.jpg", "image8.jpg", "image9.jpg"],
   },
-];
+};
+
+export default projectsData;
