630eb6e9fc1e5475171dd48cadb3798af0a7e929
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-20 00:28:07 +0200

    create projectsData exemple file

---
+++ data/projectsData.js
@@ -0,0 +1,26 @@
+const projectsData = [
+  {
+    id: "projet1",
+    nom: "Projet 1",
+    description: "Description du projet 1",
+    date: "2022-03-15",
+    tech: ["React", "Next.js", "Tailwind CSS"],
+    images: ["image1.jpg", "image2.jpg", "image3.jpg"],
+  },
+  {
+    id: "projet2",
+    nom: "Projet 2",
+    description: "Description du projet 2",
+    date: "2022-05-20",
+    tech: ["Vue.js", "Express", "MongoDB"],
+    images: ["image4.jpg", "image5.jpg", "image6.jpg"],
+  },
+  {
+    id: "projet3",
+    nom: "Projet 3",
+    description: "Description du projet 3",
+    date: "2022-08-10",
+    tech: ["Angular", "Node.js", "PostgreSQL"],
+    images: ["image7.jpg", "image8.jpg", "image9.jpg"],
+  },
+];
