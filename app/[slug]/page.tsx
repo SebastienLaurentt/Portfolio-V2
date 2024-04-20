6c2bff684c3c6880fbae04035cb042c3f8af0201
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-20 22:05:03 +0200

    update mobile css

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -52,26 +52,26 @@ export default function Page({ params }: { params: { slug: string } }) {
   return (
     <div className=" flex w-full flex-col px-6 py-20 md:px-10 lg:pt-36 xl:h-screen  xl:flex-row xl:items-center xl:px-16 ">
       {/* Textual Infos */}
-      <div className="flex flex-col justify-around py-8 xl:h-full xl:w-1/3 xl:justify-around 2xl:px-4">
+      <div className="flex flex-col justify-around py-12 xl:h-full xl:w-1/3 xl:justify-around 2xl:px-4">
         {/* First Div : Name and Date  */}
         <div>
-          <h2 className="mb-3 text-left text-5xl md:text-6xl lg:text-8xl xl:mb-0 xl:flex-col xl:text-8xl xl:leading-[68px] 2xl:text-11xl 2xl:leading-[88px]">
+          <h2 className="mb-4 text-left text-6xl lg:text-8xl xl:mb-0 xl:flex-col xl:text-8xl xl:leading-[68px] 2xl:text-11xl 2xl:leading-[88px]">
             {project.name}
           </h2>
         </div>
 
         {/* Second Div : Description, Tech Stack and Links */}
-        <div className="flex flex-col gap-y-1 2xl:gap-y-4">
+        <div className="flex flex-col gap-y-4">
           {/* Description */}
           <div>
-            <p className="w-[320px] text-lg leading-6 md:w-[480px] md:text-xl md:leading-7 lg:w-[540px] lg:leading-8 xl:w-[300px] xl:text-lg xl:leading-5 2xl:w-[400px]  2xl:text-xl 2xl:leading-6">
+            <p className="w-[320px] text-xl leading-7 md:w-[480px] md:text-xl md:leading-7 lg:w-[540px] lg:text-2xl lg:leading-8 xl:w-[300px] xl:text-lg xl:leading-5 2xl:w-[400px]  2xl:text-xl 2xl:leading-6">
               {project.description}
             </p>
           </div>
 
           {/* Tech Stack */}
           <div>
-            <ul className="flex flex-row gap-x-4 text-md 2xl:text-md">
+            <ul className="flex flex-row gap-x-4 text-md text-primary-foreground md:text-lg lg:text-xl 2xl:text-md">
               {project.tech.map((tech) => (
                 <li key={tech}>{tech}</li>
               ))}
@@ -115,8 +115,10 @@ export default function Page({ params }: { params: { slug: string } }) {
         <ul className="flex flex-col gap-y-8">
           {project.images.map((image, index) => (
             <li key={index}>
-              <span className="text-md">{image.description}</span>
-              <Image src={image.src} alt="" className="rounded-lg" />
+              <span className="text-lg text-primary-foreground md:text-lg lg:text-xl">
+                {image.description}
+              </span>
+              <Image src={image.src} alt="" className="mt-1 rounded-lg" />
             </li>
           ))}
         </ul>
