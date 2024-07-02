ec59a2b169f91d313a69cf61550e1145cef5513e
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-02 22:34:03 +0200

    update og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ export const metadata = {
   title: "Sébastien Laurent - Développeur Web",
   description:
     "Bienvenue sur mon portfolio ! Venez découvrir mon histoire et les derniers projets que j'ai réalisés.",
-    metadataBase: new URL('https://sebastienlaurent.fr/'),
 };
 
 const Bebas = Bebas_Neue({
