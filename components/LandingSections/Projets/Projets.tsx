22dea7a9c05a5dff707e75e97bf2dd91700e8ac6
SebastienLaurentt <slaurent.26@gmail.com>
2024-07-10 14:39:02 +0200

    update monipost url

--- components/LandingSections/Projets/Projets.tsx
+++ components/LandingSections/Projets/Projets.tsx
@@ -44,7 +44,7 @@ const Projets = () => {
           projectTitle="MoniPost"
           projectDescription="Landing page pour Start-Up."
           projectStack="Next.Js • TypeScript • GSAP"
-          siteHref="https://moni-post-rmpu.vercel.app/"
+          siteHref="https://monipost.com/"
           githubHref="https://github.com/SebastienLaurentt/MoniPost"
           slug="monipost"
         />
